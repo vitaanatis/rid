d287afa8c0b7692c4791ca45d577add26d442db8
Hubble Developer <anonymous@users.noreply.github.com>
2025-09-23 17:46:06 -0700

    Updated file

--- watermark.js
+++ watermark.js
@@ -1,5 +1,18 @@
-import { rtdb } from './firebase.js';
-import { ref, onValue } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8tI2w4IselKQnAICoAcPsVryK7tXYvq8",
+  authDomain: "hubblealpha.firebaseapp.com",
+  projectId: "hubblealpha",
+  storageBucket: "hubblealpha.firebasestorage.app",
+  messagingSenderId: "1019025958368",
+  appId: "1:1019025958368:web:4b1a907be2b098c1b67785",
+  measurementId: "G-TW9QCGPN5V"
+};
+
+const app = initializeApp(firebaseConfig);
+const rtdb = getDatabase(app);
 
 class Watermark {
     constructor() {
